8de474c80faac235cb3f66b9aeaf2e168d68d082
Carlos Costa <carllos.nc@gmail.com>
2024-09-07 01:15:56 -0300

    Adding github link

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
 export { default as Navbar } from './Navbar.astro'
 export { default as Footer } from './Footer.astro'
-export { default as Header } from './Header.astro'
 export { default as Logo } from './Logo.astro'
